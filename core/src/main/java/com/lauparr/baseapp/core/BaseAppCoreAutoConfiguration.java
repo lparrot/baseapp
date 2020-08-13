b910bdbd7c0f7387223ca7b7eadf1b22c698a6e8
lauparr <laurent.parrot78@gmail.com>
2020-08-13 19:36:19 +0200

    Suppression du @PostConstruct

--- core/src/main/java/com/lauparr/baseapp/core/BaseAppCoreAutoConfiguration.java
+++ core/src/main/java/com/lauparr/baseapp/core/BaseAppCoreAutoConfiguration.java
@@ -1,23 +1,12 @@
 package com.lauparr.baseapp.core;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-
-import javax.annotation.PostConstruct;
 
 @Configuration
 @ConditionalOnWebApplication
 @EnableConfigurationProperties(BaseAppCoreProperties.class)
 public class BaseAppCoreAutoConfiguration {
-
-	@Value("${baseapp.prefix}")
-	private String prefix;
-
-	@PostConstruct
-	public void init() {
-		System.out.println("BaseAppCoreModule.init : prefix=" + this.prefix);
-	}
 
 }
